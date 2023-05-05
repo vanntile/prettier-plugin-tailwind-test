81a65d31b754347a20d076ab83ebc4be7a2e33f5
vanntile <33692650+vanntile@users.noreply.github.com>
2023-05-05 16:28:22 +0300

    init: :tada: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue,md,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#3429AA",
+          secondary: "#6A86CF",
+          tint: "#A3B5E3",
+        },
+      },
+    },
+  },
+};
